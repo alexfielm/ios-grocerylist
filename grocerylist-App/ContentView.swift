b53176ef20f45b47f6a1174eaab054bdf576ba2c
alexfielm <afielmachado@icloud.com>
2025-06-13 12:17:54 -0300

    Initial Commit

---
+++ grocerylist-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  grocerylist-App
+//
+//  Created by Alex Sandro Fiel Machado on 13/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ grocerylist-App/grocerylist_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  grocerylist_AppApp.swift
+//  grocerylist-App
+//
+//  Created by Alex Sandro Fiel Machado on 13/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct grocerylist_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
